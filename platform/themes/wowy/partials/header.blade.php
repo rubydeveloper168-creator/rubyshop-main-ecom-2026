eb87a60d2dac3357f00f7c5d7c81afb331e740dc
trynotcatcherror <garinsookkalya@gmail.com>
2026-05-30 17:02:10 +0700

    update

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -106,11 +106,11 @@
         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
         t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
         document,'script','https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1073208261615128');
+        fbq('init', '1559144322039457');
         fbq('track', 'PageView');
         </script>
         <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=1073208261615128&ev=PageView&noscript=1"/></noscript>
+        src="https://www.facebook.com/tr?id=1559144322039457&ev=PageView&noscript=1"/></noscript>
         <!-- End Meta Pixel Base Code -->
         @php
             $fallbackSeoTitle = trim(strip_tags(SeoHelper::getTitle() ?: theme_option('seo_title', theme_option('site_title', 'RUBYSHOP'))));
